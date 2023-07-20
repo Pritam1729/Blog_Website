3e24e2ecbe2a5ece47e6c6e4297931df59783cce
Pritam1729 <106548018+Pritam1729@users.noreply.github.com>
2023-07-20 23:57:51 +0530

    after backend

--- app.js
+++ app.js
@@ -4,12 +4,30 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require('mongoose')
+require("dotenv").config()
+const mongodbid = process.env.mongodbid
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+
+mongoose.connect(mongodbid)
+
+mongoose.connection.on('connected',function(){
+  console.log("Connected to database")
+})
+
+const BLogScheme = {
+    name : String,
+    para : String
+}
+
+const Blog = mongoose.model("Blog",BLogScheme)
+
+
 
 app.set('view engine', 'ejs');
 
@@ -18,9 +36,13 @@ app.use(express.static("public"));
 
 let posts = [];
 
+
+// res.render("home",{homeStarting : homeStartingContent,posts : posts});
+
 app.get("/",function(req,res){
-  res.render("home",{homeStarting : homeStartingContent,posts : posts});
-  
+    blogs = Blog.find({}).then(function(data){
+        res.render("home",{homeStarting : homeStartingContent,posts : data});
+    })
 })
 
 app.get("/about",function(req,res){
@@ -36,28 +58,22 @@ app.get("/compose",function(req,res){
 })
 
 app.get("/posts/:item",function(req,res){
-  var storeTitle = _.lowerCase(req.params.item);
-  // console.log(storeTitle);
-  posts.forEach(function(ele){
-    var title = _.lowerCase(ele.title);
-    if(storeTitle === title) {
-      res.render("post",{posts : ele,kebab : _.kebabCase(ele.title)});
-    }
-    else{
-      console.log("Not Found");
-    }
+  var storeTitle = req.params.item;
+  Blog.findOne({'_id' : storeTitle}).then(function(data){
+      // console.log(data)
+      res.render("post",{posts : data})
   })
-  // else console.log("Not Found");
+  
 })
 
 
 app.post("/compose",function(req,res){
 
-  const post = {
-    title : req.body.postTitle,
-    postText : req.body.postText
-  }
-  posts.push(post);
+  const post = new Blog({
+    name : req.body.postTitle,
+    para : req.body.postText
+  })
+  post.save()
   res.redirect("/");
 })
 
